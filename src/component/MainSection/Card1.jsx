9dc875e1273b677201b57150a3e756054e05b807
shivayeshiv <shivamkumarprajesh159@gmail.com>
2026-07-29 12:42:18 +0530

    fix App.jsx file casing and import path

--- src/component/MainSection/Card1.jsx
+++ src/component/MainSection/Card1.jsx
@@ -7,24 +7,27 @@ const Card1 = () => {
     <div>
         <hr />
         <div className="card-fluid">
-           <div className="row d-flex ">
-            <div className="col-md-3">
+           <div className="row ">
+            <div className="col-md-6" id='imagemar'>
+               <div className="div1">
                <img src={img5} alt="" />
                     <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-            <div className="col-md-3">
-                <div className="col-md-3">
-               <img src={img6} alt="" />
-                    <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+                </div>
             </div>
             
-           </div>
+                <div className="col-md-6">
+                    <div className="div2">
+                <img src={img6} alt="" className=''/>
+                        <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>    
         </div>
+        
     </div>
   )
 }
