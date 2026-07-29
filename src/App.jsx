b013835014ede597ec099e5b1b2b4d8fcff51e0c
shivayeshiv <shivamkumarprajesh159@gmail.com>
2026-07-29 11:31:24 +0530

    Fix file import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from "./component/Navbar/Navbar.jsx"
 import Main1 from './component/MainSection/Mainsection.jsx'
-import About from './component/MainSection/about.jsx'
+import About from './component/MainSection/About.jsx'
 import Card1 from './component/MainSection/Card1.jsx'
 const App = () => {
   return (
