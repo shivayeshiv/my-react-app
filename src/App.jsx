96acf3de3c8f6c1839231801b0e8b0405b976ce5
shivayeshiv <shivamkumarprajesh159@gmail.com>
2026-07-29 12:05:49 +0530

    fix App.jsx file casing and import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from "./component/Navbar/Navbar.jsx"
 import Main1 from './component/MainSection/Mainsection.jsx'
-import About from './component/MainSection/About.jsx'
+import About from './component/MainSection/About1.jsx'
 import Card1 from './component/MainSection/Card1.jsx'
 const App = () => {
   return (
