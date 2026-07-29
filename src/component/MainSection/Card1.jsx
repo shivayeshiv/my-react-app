338e09ea120eddd52df54170091d043901260084
shivayeshiv <shivamkumarprajesh159@gmail.com>
2026-07-29 13:35:48 +0530

    fix App.jsx file casing and import path

--- src/component/MainSection/Card1.jsx
+++ src/component/MainSection/Card1.jsx
@@ -9,21 +9,17 @@ const Card1 = () => {
         <div className="card-fluid">
            <div className="row ">
             <div className="col-md-6" id='imagemar'>
-               <div className="div1">
                <img src={img5} alt="" />
                     <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
             </div>
             
                 <div className="col-md-6">
-                    <div className="div2">
                 <img src={img6} alt="" className=''/>
                         <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
                 </div>
             </div>    
         </div>
