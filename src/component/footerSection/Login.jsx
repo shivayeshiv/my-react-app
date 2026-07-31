5426589f976016495c58d0ac70defbca337de704
shivayeshiv <shivamkumarprajesh159@gmail.com>
2026-07-31 22:55:59 +0530

    Describe your changes

--- src/component/footerSection/Login.jsx
+++ src/component/footerSection/Login.jsx
@@ -1,34 +1,32 @@
 import React from 'react'
-
+import img1 from "../../assets/images/23.png"
 const Login = () => {
   return (
     <div>
-        <div className="request-fluid" id='login'>
-            <form action="">
-                <div className="req row">
-                    <div className="col-md-6">
-                    <h1>Request Demo</h1>
+        <hr />
+        <div className="from-fluid" id='Shiv'>
+            <div className="row">
+                <div className="col-md-7 hl" >
+                    <form action="">
+                        <h1 className='text-center m-4 '>REQUEST FROM</h1>
+                        <div className='Name'>
+                        <input type="text" placeholder='Name' />
+                        <input type="email" name="Email" id="" placeholder='Email Id' />
+                        </div>
+                        <div className='details'>
+                            <input type="number" name="" id="" Phone No placeholder='Require of Toilet' />
+                            <input type="text" placeholder='city' />
+                        </div>
+                        <input className='Hlo' type="text" placeholder='Enter the Require of NO Toilet' />
+                        <input className='Hlo' type="text" placeholder='Write a Massage Here' />
+                        <button>SUBMITTED</button>
+                    </form>
                 </div>
-                <div className="main">
-                    <div>
-                    <input type="text" placeholder='Name' />
-                    <input type="email" name="Email" id="" placeholder='Email Id' />
-                    </div>
-                    <div>
-                        <input type="number" name="" id="" Phone No />
-                        <input type="text" placeholder='city' />
-                    </div>
-                    <input type="text" placeholder='Enter the Require of NO Toilet' />
-                    <input type="text" placeholder='Write a Massage Here' />
+                <div className="col-md-5">
+                    <img src={img1} alt="" />
                 </div>
-                </div>
-
-                <div className="col-md-6">
-                    hallo
-                </div>
-            </form>
+            </div>
         </div>
-
     </div>
   )
 }
